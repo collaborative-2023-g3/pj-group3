ee453debf430134640eb736e89c945fb09d0aaf5
Seiji KK <sayzkkgw@gmail.com>
2024-01-03 20:46:01 +0900

    react-router-domによるフロント側のルーティング設定の変更

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,19 @@
 // NOTIE : このファイルは触らないでください
 // Entry point for the build script in your package.json
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import Top from './pages/Top';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import Top from "./pages/Top";
+import AuthRegister from "./pages/AuthRegister";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Header } from "./templates/Header";
 
-const root = createRoot(document.getElementById('root'));
-root.render(<Top />);
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <BrowserRouter>
+    <Header />
+    <Routes>
+      <Route path="/" element={<Top />} />
+      <Route exact path="/register" element={<AuthRegister />} />
+    </Routes>
+  </BrowserRouter>
+);
--- app/javascript/templates/Header/index.tsx
+++ app/javascript/templates/Header/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export const Header: React.FC = () => {
   return (
@@ -6,7 +7,7 @@ export const Header: React.FC = () => {
       <h1 className="t-header__title">Cat Family</h1>
       <ul className="t-header__link">
         <li><a href="/">ログイン</a></li>
-        <li><a href="/">会員登録</a></li>
+        <li><Link to="/register"> 会員登録</Link></li>
       </ul>
     </div>
   );
