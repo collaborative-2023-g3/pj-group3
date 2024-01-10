8ecb7b09ab360179ce4dec8518bd771cd1ba5a68
junichi-fukushima <71912259+junichi-fukushima@users.noreply.github.com>
2024-01-10 22:07:49 +0900

    カイゼン

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,11 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /myapp/tmp/pids/server.pid
 
+# DB作成
+bundle exec rails db:create
+
+# マイグレーション処理
+bundle exec rails db:migrate
+
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
