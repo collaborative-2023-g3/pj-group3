cc2d659c3ef96c5b81c375b5596d579ad353b6de
Seiji KK <sayzkkgw@gmail.com>
2024-01-12 13:07:54 +0900

    アカウント登録用アクション追加

--- app/controllers/v1/auth/registrations_controller.rb
+++ app/controllers/v1/auth/registrations_controller.rb
@@ -1,6 +1,12 @@
 module V1
   module Auth
     class RegistrationsController < DeviseTokenAuth::RegistrationsController
+
+      private
+
+      def sign_up_params
+        params.permit(:email, :password, :password_confirmation, :user_name, :user_type)
+      end
     end
   end
 end
