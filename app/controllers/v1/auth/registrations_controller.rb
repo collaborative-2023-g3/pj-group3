51c41030315cbba45d3f8645daf30e33d460314a
Seiji KK <sayzkkgw@gmail.com>
2024-01-14 14:26:49 +0900

    Rubocopのチェックへの修正

--- app/controllers/v1/auth/registrations_controller.rb
+++ app/controllers/v1/auth/registrations_controller.rb
@@ -2,6 +2,7 @@ module V1
   module Auth
     class RegistrationsController < DeviseTokenAuth::RegistrationsController
       private
+
       def sign_up_params
         params.permit(:email, :password, :password_confirmation, :user_name, :user_type)
       end
