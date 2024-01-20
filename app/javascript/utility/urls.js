cb931b2d45bf73645b6a8885c1a2e5485be8ed65
junichi-fukushima <71912259+junichi-fukushima@users.noreply.github.com>
2024-01-20 18:51:15 +0900

    環境変数のセット(URL)

--- app/javascript/utility/urls.js
+++ app/javascript/utility/urls.js
@@ -1,12 +1,13 @@
 // APIを取得するURL一式
-export const DEFAULT_API = process.env.API_ENDPOINT || 'http://localhost:3000/api';
+const API_ENDPOINT = window.environmentVariables.API_ENDPOINT;
+export const DEFAULT_API_ENDPOINT = API_ENDPOINT || 'http://localhost:3000/api';
 
 // ユーザー管理機能
-export const signUpUrl = `${DEFAULT_API}/auth`
-export const successURL = `${DEFAULT_API}/signup/complete`
-export const signInURL = `${DEFAULT_API}/auth/sign_in`
+export const signUpUrl = `${DEFAULT_API_ENDPOINT}/auth`
+export const successURL = `${DEFAULT_API_ENDPOINT}/signup/complete`
+export const signInURL = `${DEFAULT_API_ENDPOINT}/auth/sign_in`
 export const signOutURL = `/auth/sign_out`
-export const sessionURL =  `${DEFAULT_API}/auth/sessions`
+export const sessionURL =  `${DEFAULT_API_ENDPOINT}/auth/sessions`
 
 // 検索&&ソート機能
-export const catSearchUrl = `${DEFAULT_API}/cats/search`
+export const catSearchUrl = `${DEFAULT_API_ENDPOINT}/cats/search`
