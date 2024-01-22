239afef542d27a4fffd0180621cdbc07a2899d73
junichi-fukushima <71912259+junichi-fukushima@users.noreply.github.com>
2024-01-22 12:22:21 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
       # prefix :http://localhost:3000/api/v1/auth/***
       mount_devise_token_auth_for 'User', at: 'auth', controllers: {
         registrations: 'api/v1/auth/registrations',
-        sessions: 'api/v1/auth/sessions'
+        sign_in: "api/v1/auth/sign_in"
       }
 
       # devise以外のルーティング
