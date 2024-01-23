7a15a7a228823a5ddc5dfe1362c7d8ad2b8f5455
junichi-fukushima <71912259+junichi-fukushima@users.noreply.github.com>
2024-01-23 20:31:18 +0900

    途中まで

--- app/javascript/pages/Top/index.tsx
+++ app/javascript/pages/Top/index.tsx
@@ -12,6 +12,18 @@ const Top: React.FC = () => {
       <PageTemplate>
           <H2Header>猫募集一覧</H2Header>
           <p className="p-top__description">お気に入りの猫を見つけよう♪</p>
+          <div className='p-top__container'>
+            <div className="p-top__serch">ここは検索エリアどす。。。</div>
+            <ul className="p-top__cat-list">
+              <li>
+                <div><img src="http://pj-5bucket.s3.ap-northeast-1.amazonaws.com/test-cat.png" alt="猫の写真" /></div>
+                <p>ねこ太郎</p>
+                <p>2023年1月1日生まれ</p>
+                <p>男の子</p>
+                <p>東京都</p>
+              </li>
+            </ul>
+          </div>
       </PageTemplate>
     </ContainerTemplate>
   );
