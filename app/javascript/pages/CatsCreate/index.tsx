b86b446151d35d6148fc3c622d107bde0ff35681
Seiji KK <sayzkkgw@gmail.com>
2024-01-24 23:55:17 +0900

    fix format a bit

--- app/javascript/pages/CatsCreate/index.tsx
+++ app/javascript/pages/CatsCreate/index.tsx
@@ -68,7 +68,7 @@ export const CatsCreate: React.FC = () => {
 
             if (response.ok) {
                 // todo
-                // console.log('success!');
+
             } else {
                 throw new Error("サーバーエラーが発生しました。再度お試しください。");
             }
@@ -79,7 +79,7 @@ export const CatsCreate: React.FC = () => {
 
     return (
 
-        < ContainerTemplate >
+        <ContainerTemplate>
             <Header />
             <PageTemplate>
                 <H2Header>猫の登録</H2Header>
