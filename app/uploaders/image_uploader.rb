735b8bea008d7e04fa6da679c5b78a0473768d58
kz.ogawa <kz.ogawa14@gmail.com>
2024-01-26 00:28:17 +0900

    Rubocop対応

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -43,11 +43,11 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
   def filename
-  #   "something.jpg"
     "#{secure_token}.#{file.extension}" if original_filename.present?
   end
 
   protected
+
   def secure_token
     var = :"@#{mounted_as}_secure_token"
     model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.uuid)
