6799aea57070b379aeb86b181d7d2001e1da2ad4
kz.ogawa <kz.ogawa14@gmail.com>
2024-01-26 00:24:03 +0900

    アップロードされるファイル名をランダムなものに変更する処理を実装

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -42,7 +42,14 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  # def filename
+  def filename
   #   "something.jpg"
-  # end
+    "#{secure_token}.#{file.extension}" if original_filename.present?
+  end
+
+  protected
+  def secure_token
+    var = :"@#{mounted_as}_secure_token"
+    model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.uuid)
+  end
 end
