5801c930ecc221c3bbef22f0fea433663c3514dd
junichi-fukushima <71912259+junichi-fukushima@users.noreply.github.com>
2024-01-26 20:07:17 +0900

    画像pushできたので更新

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,14 +2,14 @@ CarrierWave.configure do |config|
   if Rails.env.production?
     config.storage = :fog
     config.fog_provider = 'fog/aws'
+    config.fog_directory = "pj-5bucket"
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/pj-5bucket'
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: Rails.application.credentials.aws[:ACCESS_KEY_ID],
       aws_secret_access_key: Rails.application.credentials.aws[:SECRET_ACCESS_KEY],
       region: 'ap-northeast-1'
     }
-    config.fog_directory = "/"
-    config.asset_host = "http://pj-5bucket.s3.ap-northeast-1.amazonaws.com/"
   else
     config.asset_host = "http://localhost:3000"
     config.storage = :file
